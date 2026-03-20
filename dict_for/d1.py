91fa6aa832597f7681684a360748b34c41497ed5
Sayali Hatwar <sayali.hatwar4321@gmail.com>
2026-03-20 18:02:38 +0530

    lis, dict, set and tuple example

---
+++ dict_for/d1.py
@@ -0,0 +1,9 @@
+student = {
+    'name':'sayali',
+    'age':26,
+    'course':'Computer Science'
+}
+
+for stud in student:
+    print(f"stud = {stud}, type of stud = {type(stud)}, keyvalue = {student.get(stud)}")
+
